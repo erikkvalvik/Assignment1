a24431365d96c4437e8941829608b30ce6c8a95b
erikkvalvik <42551121+erikkvalvik@users.noreply.github.com>
2022-01-17 09:05:54 +0100

    Initial commit

---
+++ Assignment1/RPGCharacters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment1
+{
+    public class RPGCharacters
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
